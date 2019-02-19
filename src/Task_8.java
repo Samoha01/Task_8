6eac7d798d9fe8c393820fa37ed2852019730aff
samoha01 <samoha1@list.ru>
2019-02-19 10:36:02 +0300

    Task 8 done

---
+++ src/Task_8.java
@@ -0,0 +1,10 @@
+/*
+   Задание № 8
+Необходимо с помощью цикла вывести на экран числа от 1 до 50 */
+public class Task_8 {
+    public static void main(String args[]){
+        for (int j = 1; j <= 50; j++) {
+            System.out.print (j + " ");
+        }
+    }
+}
